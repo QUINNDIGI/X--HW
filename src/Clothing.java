a598d352089e14ed5a9269befe8f0c4188535fee
Quinn di Girolamo <quinndigi@gmail.com>
2022-06-27 16:11:37 -0400

    Clothing Constructor HW

---
+++ src/Clothing.java
@@ -0,0 +1,37 @@
+public class Clothing {
+
+    public double price;
+    public String type;
+    public String size;
+
+
+    public Clothing() {
+        price = 29.9;
+        type = "T-shirt";
+        size = "Medium";
+    }
+
+    public Clothing(double price, String type, String size) {
+            this.price = price;
+            this.type = type;
+            this.size = size;
+
+    }
+
+    public void increasePrice() {
+        price += 5;
+    }
+
+    public void increasePrice(double x) {
+        price += x;
+    }
+
+    public void details() {
+        System.out.println("Item: " + type);
+        System.out.println("Size: " + size);
+        System.out.println("Price: $" + price);
+        System.out.println();
+
+    }
+
+}
