5ff0044b3bc0dbe4cd3fd97c1f890d1e9bce53c0
Quinn di Girolamo <quinndigi@gmail.com>
2022-07-01 16:23:33 -0400

    HW Problem 5 finally

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,24 +1,38 @@
-public class Reverse
-{
-    public static void main(String[] args)
-    {
-      /*
-      System.out.println(isExactReverse("ba", "a"));
-      System.out.println(isExactReverse("desserts", "stressed"));
-      System.out.println(isExactReverse("apple", "apple"));
-      System.out.println(isExactReverse("regal", "lager"));
-      System.out.println(isExactReverse("war", "raw"));
-      System.out.println(isExactReverse("pal", "slap"));
-      */
+public class Reverse {
+    public static void main(String[] args) {
+
+        System.out.println(isExactReverse("ba", "a"));
+        System.out.println(isExactReverse("desserts", "stressed"));
+        System.out.println(isExactReverse("apple", "apple"));
+        System.out.println(isExactReverse("regal", "lager"));
+        System.out.println(isExactReverse("war", "raw"));
+        System.out.println(isExactReverse("pal", "slap"));
+
 
     }
-//    public static boolean isExactReverse(String x, String y)
-//    {
-//        if (x.length() == y.length()){
-//
-//        }
-//
-//
-//    }
 
+    public static boolean isExactReverse(String x, String y) {
+
+        if(x.length() != y.length()) {
+            return false;
+        }
+
+        int xI = 0;
+
+
+        for (int i = y.length() - 1; i >= 0; i--) {
+
+            char yVal = y.charAt(i);
+            char xVal = x.charAt(xI);
+
+            if (xVal == yVal) {
+                xI++;
+
+            } else {
+                return false;
+
+            }
+        }
+        return true;
+    }
 }
